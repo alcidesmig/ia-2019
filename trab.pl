ba145dc12932c702b9f0785c2774965d772dcbd1
Alcides Mignoso e Silva <alcidesmig@gmail.com>
2019-10-20 21:58:35 -0300

    Correção

--- trab.pl
+++ trab.pl
@@ -1,3 +1,22 @@
+escada(0, 2).
+escada(0, 8).
+escada(1, 3).
+escada(1, 9).
+escada(2, 0).
+escada(2, 6).
+escada(3, 3).
+escada(3, 8).
+
+bloqueio(2, 4).
+bloqueio(4, 5).
+
+pedra(0, 1).
+pedra(0, 5).
+pedra(1, 5).
+pedra(3, 2).
+pedra(3, 9).
+pedra(4, 0).
+
 % Limites da matriz
 limiteI(X) :- X < 5, X >= 0.						% Estipula o limite de linhas
 limiteJ(X) :- X < 10, X >= 0.						% Estipula o limite de colunas
@@ -16,7 +35,7 @@ andarPedra(I, J, Fogos) :- 							% Verifica se pode andar (em relação a pedra
 	;												% ou
 	(
 		pedra(I, J),								% Existe pedra na nova posição
-		NJ = NovoJ + 1,
+		NJ = I + 1,
 		livre(I, NJ, Fogos)							% Mas a posterior está livre. Obs: posições inválidas contam como livres
 	).
 
